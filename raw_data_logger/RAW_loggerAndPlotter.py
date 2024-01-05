4a7b8c8fe79fc79e0fc3cb55682ed5c897dc08d6
Angel Lozano <angel_ivan_5@outlook.com>
2024-01-04 23:28:07 -0600

    logging signed RAW

--- raw_data_logger/RAW_loggerAndPlotter.py
+++ raw_data_logger/RAW_loggerAndPlotter.py
@@ -4,16 +4,18 @@
 
 import serial
 import numpy
+import tkinter as tk
 
 board = serial.Serial( port="/dev/ttyACM0", baudrate=115200, timeout=1 )
 
-samples_to_read = 500
+samples_to_read = 2000
 
 i = 0
 lines = []
 skip_lines = 4
 processed_data1 = []
 final_data = [
+    [],
     [],
     [],
     [],
@@ -37,32 +39,39 @@ for processed_line in processed_data1:
     actual_processed_line = processed_line.split(':')
     for i in range( 3 ):
         final_data[i].append( int(actual_processed_line[i]) )
-    final_data[3].append( sample )
+    final_data[4].append( sample )
     sample += 1
 
+for i in range( len(final_data[0]) ):
+    if( final_data[1][i] & 0x80 == 128 ):
+        final_data[3].append( (( final_data[1][i] ^ 0x80) * 256 + final_data[2][i]) * -1 )
+    else:
+        final_data[3].append( final_data[1][i] * 256  + final_data[2][i] )
 
 def cli_print():
     print( 32*"-" + " RESULTS " + 32*"-" )
     print("TOTAL samples=",len(final_data[0]))
-    print( "Sample\tQuality\tH_byte\tL_byte" )
+    print( "Sample\tQuality\tH_byte\tL_byte\tReal" )
     for i in range(len(final_data[0])):
-        print( "{0}\t{1}\t{2}\t{3}".format(
-            final_data[3][i],
+        print( "{0}\t{1}\t{2}\t{3}\t{4}".format(
+            final_data[4][i],
             final_data[0][i],
             final_data[1][i],
-            final_data[2][i]
+            final_data[2][i],
+            final_data[3][i]
             ) )
         
 
 def savelog_csv():
     numpy.savetxt(
         "/home/ailr16/Documents/raw.csv",
-        numpy.c_[final_data[3],
+        numpy.c_[final_data[4],
                  final_data[0],
                  final_data[1],
-                 final_data[2]],
+                 final_data[2],
+                 final_data[3]],
                  delimiter=",",
-                 header="Sample,Quality,H_byte,L_byte"
+                 header="Sample,Quality,H_byte,L_byte,Real"
     )
 
 cli_print()
