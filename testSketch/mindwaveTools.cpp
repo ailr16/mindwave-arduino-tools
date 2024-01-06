7b7be31aa959e0a754cdd081b7494d1c81b70f6a
Angel Lozano <angel_ivan_5@outlook.com>
2024-01-06 17:50:28 -0600

    Added class for handling headset

---
+++ testSketch/mindwaveTools.cpp
@@ -0,0 +1,28 @@
+#include "mindwaveTools.h"
+
+unsigned int MindwaveHeadset::getAttention()
+{
+  return this->attentionValue;
+}
+
+unsigned int MindwaveHeadset::getMeditation()
+{
+  return this->meditationValue;
+}
+
+int MindwaveHeadset::getRaw()
+{
+  return this->rawValue;
+}
+
+void MindwaveHeadset::getAllRaw(long *allRawArray)
+{
+  allRawArray[ALLRAW_OUTPUT_DELTA_INDEX]      = this->allRawArray[0]
+  allRawArray[ALLRAW_OUTPUT_THETA_INDEX]      = this->allRawArray[1]
+  allRawArray[ALLRAW_OUTPUT_LOW_APLHA_INDEX]  = this->allRawArray[2]
+  allRawArray[ALLRAW_OUTPUT_HIGH_APLHA_INDEX] = this->allRawArray[3]
+  allRawArray[ALLRAW_OUTPUT_LOW_BETA_INDEX]   = this->allRawArray[4]
+  allRawArray[ALLRAW_OUTPUT_HIGH_BETA_INDEX]  = this->allRawArray[5]
+  allRawArray[ALLRAW_OUTPUT_LOW_GAMMA_INDEX]  = this->allRawArray[6]
+  allRawArray[ALLRAW_OUTPUT_MID_GAMMA_INDEX]  = this->allRawArray[7]
+}
---
+++ testSketch/mindwaveTools.h
@@ -0,0 +1,40 @@
+#ifndef MINDWAVE_TOOLS_H
+#define MINDWAVE_TOOLS_H
+
+#define SYNC_BYTE_1   0xAA
+#define SYNC_BYTE_2   0xAA
+
+#define DATA_QUALITY_LED    13
+
+#define CODE_SIGNAL_QUALITY 0x02
+#define CODE_ATTENTION      0x04
+#define CODE_MEDITATION     0x05
+#define CODE_RAW_WAVE_VALUE 0x80
+#define CODE_ASIC_EEG_POWER 0x83
+
+#define ALLRAW_OUTPUT_DELTA_INDEX      0
+#define ALLRAW_OUTPUT_THETA_INDEX      1
+#define ALLRAW_OUTPUT_LOW_APLHA_INDEX  2
+#define ALLRAW_OUTPUT_HIGH_APLHA_INDEX 3
+#define ALLRAW_OUTPUT_LOW_BETA_INDEX   4
+#define ALLRAW_OUTPUT_HIGH_BETA_INDEX  5
+#define ALLRAW_OUTPUT_LOW_GAMMA_INDEX  6
+#define ALLRAW_OUTPUT_MID_GAMMA_INDEX  7
+
+class MindwaveHeadset
+{
+  private:
+    unsigned int qualityValue;
+    unsigned int attentionValue;
+    unsigned int meditationValue;
+    int rawValue;
+    long allRawArray[8];
+
+  public:
+    unsigned int getAttention( void );
+    unsigned int getMeditation( void );
+    int getRaw( void );
+    void getAllRaw( long *allRawArray );
+};
+
+#endif
